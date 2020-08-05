d621c0e1e6a319449cea3b269bc16fee5da53387
Hristo-Cholakov <60668772+Hristo-Cholakov@users.noreply.github.com>
2020-08-05 16:05:56 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,83 @@
+//var gameStarted = false;
+var level = 0;
+var colors = ["red", "blue", "yellow", "green"];
+var gameSuggestedPattern = [];
+var userClickedPattern = [];
+
+$(document).keypress(function(){
+
+level = 0;
+//gameStarted = true;
+gameSuggestedPattern = [];
+userClickedPattern = [];
+  gamePlay();
+
+
+
+});
+
+$(".btn").click(function(){
+  console.log("click method");
+  userChosenColor = $(this).attr("id");
+  customAnimation(userChosenColor);
+  playSound(userChosenColor);
+  userClickedPattern.push(userChosenColor);
+  checkSequence(userClickedPattern.length-1);
+
+})
+function playSound(buttonName){
+  var audio = new Audio('sounds/'+ buttonName + '.mp3');
+  audio.play();
+};
+
+function customAnimation(button){
+  $("#"+button).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function gamePlay(){
+
+  level++;
+  $("h1").text("Level " + level);
+  var randomPick = Math.floor(Math.random()*4);
+  var selectedColor = colors[randomPick];
+  gameSuggestedPattern.push(selectedColor);
+  playSound(selectedColor);
+  customAnimation(selectedColor);
+
+};
+
+function checkSequence(currentLevel){
+  // console.log("checkSeq");
+  // console.log(gameSuggestedPattern);
+  // console.log(userClickedPattern);
+  if(gameSuggestedPattern[currentLevel] === userClickedPattern[currentLevel]){
+    if(gameSuggestedPattern.length === userClickedPattern.length){
+      setTimeout(function() {
+        gamePlay();
+      }, 1000);
+      userClickedPattern = [];
+    }
+  }else{
+    wrongAnswer();
+  }
+
+};
+
+function wrongAnswer(){
+  // gameStarted = false;
+  // level = 0;
+  $("h1").text("Press any key to start the game");
+  // gameSuggestedPattern = [];
+  // userClickedPattern = [];
+  $("body").addClass("game-over");
+  setTimeout(function(){
+    $("body").removeClass("game-over");
+  }, 1500);
+  playSound('wrong');
+
+  setTimeout(function(){
+    location.reload();
+  }, 1500);
+
+
+}
